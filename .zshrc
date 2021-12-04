1fcd72ea8155f2f6fca45ee62084028496e5f1aa
aadi350 <a.aadi380@gmail.com>
2021-12-04 14:54:04 -0400

    Add pull-update and search

---
+++ .zshrc
@@ -0,0 +1,13 @@
+# Use powerline
+USE_POWERLINE="true"
+# Source manjaro-zsh-configuration
+if [[ -e /usr/share/zsh/manjaro-zsh-config ]]; then
+  source /usr/share/zsh/manjaro-zsh-config
+fi
+# Use manjaro zsh prompt
+if [[ -e /usr/share/zsh/manjaro-zsh-prompt ]]; then
+  source /usr/share/zsh/manjaro-zsh-prompt
+fi
+fpath+=${ZDOTDIR:-~}/.zsh_functions
+
+FORMAT="\nID\t{{.ID}}\nIMAGE\t{{.Image}}\nCOMMAND\t{{.Command}}\nCREATED\t{{.RunningFor}}\nSTATUS\t{{.Status}}\nPORTS\t{{.Ports}}\nNAMES\t{{.Names}}\n"
--- usr/local/bin/update.sh
+++ usr/local/bin/update.sh
@@ -7,15 +7,15 @@ echo "Enter commit message"
 read MSG
 
 # update config
-rsync --update -raz --progress /home/aadi/.config/qtile/ /home/aadi/linux/home/aadi/.config/qtile/
-rsync --update -raz --progress /home/aadi/.config/alacritty/ /home/aadi/linux/home/aadi/.config/alacritty/
-rsync --update -raz --progress "/home/aadi/.config/Code - Insiders/User/settings.json" /home/aadi/linux/home/aadi/.config/Code/User/
+rsync --update -raz --progress /home/aadi/.config/qtile/ /home/aadi/linux/.config/qtile/
+rsync --update -raz --progress /home/aadi/.config/alacritty/ /home/aadi/linux/.config/alacritty/
+# rsync --update -raz --progress "/home/aadi/.config/Code - Insiders/User/settings.json" /home/aadi/linux/.config/Code/User/
 
 # update .zshrc
-rsync --update -raz --progress /home/aadi/.zshrc /home/aadi/linux/home/aadi/
+rsync --update -raz --progress /home/aadi/.zshrc /home/aadi/linux/
 
 # update emacs
-rsync --update -raz --progress /home/aadi/.doom.d/ /home/aadi/linux/home/aadi/doom.d/
+rsync --update -raz --progress /home/aadi/.doom.d/ /home/aadi/linux/.doom.d/
 
 
 cd /home/aadi/linux && git add . && git commit -m "$MSG" && git push origin main
