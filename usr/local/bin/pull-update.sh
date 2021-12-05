aac8d06a69ecd8f6a833ef611d0c4ffd27323aca
aadi350 <a.aadi380@gmail.com>
2021-12-04 20:43:02 -0400

    Update widget font

--- usr/local/bin/pull-update.sh
+++ usr/local/bin/pull-update.sh
@@ -3,12 +3,15 @@
 cd /home/aadi/linux && git pull origin main
 
 # update config
-rsync --update -raz --progress /home/aadi/linux/.config/qtile/ /home/aadi/.config/
-rsync --update -raz --progress /home/aadi/linux/.config/alacritty/ /home/aadi/.config/alacritty/
+rsync --update -raz --progress /home/aadi/linux/home/aadi/.config/qtile/ /home/aadi/.config/qtile
+rsync --update -raz --progress /home/aadi/linux/home/aadi/.config/alacritty/ /home/aadi/.config/alacritty/
 # rsync --update -raz --progress "/home/aadi/.config/Code - Insiders/User/settings.json" /home/aadi/linux/.config/Code/User/
 
 # update .zshrc
-rsync --update -raz --progress /home/aadi/linux/.zshrc  /home/aadi/
+rsync --update -raz --progress /home/aadi/linux/home/aadi/.zshrc  /home/aadi/
 
 # update emacs
-rsync --update -raz --progress  /home/aadi/linux/.doom.d/  /home/aadi/.doom.d/
+rsync --update -raz --progress  /home/aadi/linux/home/aadi/.doom.d/  /home/aadi/.doom.d/
+
+sudo rsync --update -raz --progress  /home/aadi/linux/usr/bin/homepage /usr/bin/
+sudo rsync --update -raz --progress  /home/aadi/linux/usr/local/bin/ /usr/local/bin/
