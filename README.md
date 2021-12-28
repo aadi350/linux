d223c555e640dd1c8424f14344239f5b074dc2f1
aadi350 <a.aadi380@gmail.com>
2021-12-28 14:54:59 -0400

    add doom-emacs

--- README.md
+++ README.md
@@ -4,8 +4,20 @@ Full disclaimer, there is no guarantee that this code is bug-free, this is what 
 
 ## Install Script
 
+This assumes that you cloned the GitHub repo into your `$HOME` directory.
+If you don't know where your `$HOME` directory is, this isn't for you.
+
+### Applications Installed 
+- Nitrogen: wallpaper client
+- Zoom: video conferencing
+- Emacs: text editor
+- Doom Emacs: Evil mode in Emacs everywhere
 ## qtile Configuration
 
+[comment]: # (TODO: need to update my look and feel)
+
 ## Doom Emacs
+![Screenshot](assets/doom-emacs.png)
+
 
 ## Custom Update Files
--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@ mkdir ~/.install
 git clone https://aur.archlinux.org/zoom.git ~/.install
 makepkg -si ~/.install/zoom
 
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh ~/.install
-sh ~/.install/install.sh
+mkdir ~/.install/zoom
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh ~/.install/zoom
+sh ~/.install/zoom/install.sh
 
 pacman -S git base-devel
 pacman -S qtile
@@ -13,5 +14,19 @@ pacman -S dmenu
 pamac build flameshot
 pamac install slack-desktop --noconfirm
 pamac install inotify-tools --noconfirm
+pamac install nitrogen --noconfirm
+pacman install emacs --noconfirm
 
+mkdir ~/.install/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-py38_4.10.3-Linux-x86_64.sh ~/.install/miniconda3
+sh ~/.install/anaconda3/Miniconda3-latest-Linux-x86_64.sh
+
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install
+
+echo "~/.emacs.d/bin:$PATH"
+
+sudo cp -r ./.doom.d/* ~/.doom.d/
+sudo cp -r ./home/aadi/* ~/
+sudo cp -r ./usr/ /usr/
 sudo cp -r ~/linux/usr/local/bin/* /usr/local/bin/
